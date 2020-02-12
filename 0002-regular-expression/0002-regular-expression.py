8551ba551209e73b1c1fa42fb2c2a4f03587340a
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-11 22:32:37 -0300

    Biblioteca regex para tratar strings

--- 0002-regular-expression/0002-regular-expression.py
+++ 0002-regular-expression/0002-regular-expression.py
@@ -16,7 +16,18 @@ def isPhoneNumber(text):
 			return False
 	return True
 
-print('415-555-4242 is a phone number:')
-print(isPhoneNumber('415-555-4242'))
-print('Moshi moshi is a phone number:')
-print(isPhoneNumber('Moshi moshi'))
+# print('415-555-4242 is a phone number:')
+# print(isPhoneNumber('415-555-4242'))
+# print('Moshi moshi is a phone number:')
+# print(isPhoneNumber('Moshi moshi'))
+
+# *** #
+
+# módulo que contém métodos para tratar com regex
+import re
+
+# r antes da regex (raw string) indica que não deve escapar o texto em frente
+# .compile cria uma regex
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 415-555-4242.')
+print('Phone number found: ' + mo.group())
